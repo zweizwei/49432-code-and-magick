a2ab41cb971be3e4b93d942c9cd8c3fa04dcfe16
zweizwei <elena.ganenkova@gmail.com>
2015-10-20 01:24:27 +0300

    Task 5/1 - set timeout for scroll-parallax (with debugging yet)

---
+++ js/game_demo.js
@@ -0,0 +1,65 @@
+(function (){
+
+    var clouds = document.querySelector('.header-clouds');
+
+
+    function isContainerInTheWindow() {
+        return clouds.getBoundingClientRect().bottom > 0;
+    }
+
+    function turnCloudsParallaxOff() {
+        window.dispatchEvent(new CustomEvent('stopParallax'));
+    }
+
+    function showClouds() {
+        window.dispatchEvent(new CustomEvent('startParallax'));
+    }
+
+
+    function stopParallax() {
+
+            window.removeEventListener('scroll', cloudsPosition);
+
+    }
+
+    function startParallax() {
+
+            window.addEventListener('scroll', cloudsPosition);
+
+    }
+
+    function cloudsPosition(){
+        clouds.style.backgroundPosition = "-" + scrollY/10 + '%' + '0%';
+    }
+
+
+    function initScroll() {
+
+        var scrollTimeout;
+
+        window.addEventListener('scroll', function () {
+            clearTimeout(scrollTimeout)
+            scrollTimeout = setTimeout(checkParallax, 100);
+        })
+
+        window.addEventListener('startParallax', startParallax);
+        window.addEventListener('stopParallax', stopParallax);
+
+    }
+
+
+    function checkParallax() {
+
+        if (isContainerInTheWindow()) {
+            console.log('start showing clouds');
+            showClouds();
+        } else {
+            console.log('stop showing clouds');
+            turnCloudsParallaxOff();
+        }
+    }
+
+
+    initScroll();
+
+    })();
