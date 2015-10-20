9f408caa06cc227e98138e2d74f1e98365eb889d
zweizwei <elena.ganenkova@gmail.com>
2015-10-20 14:22:44 +0300

    Task 5/1 - add gallery js - with debugging

--- js/gallery.js
+++ js/gallery.js
@@ -1,56 +1,53 @@
 (function() {
 
-  var key = {
-    'LEFT': 37,
-    'RIGHT': 39,
-    'ESC': 27
-  };
+          var key = {
+            'LEFT' : 37,
+            'RIGHT' : 39,
+            'ESC' : 27
+      }
 
   var galleryContainer = document.querySelector('.photogallery');
   var galleryElement = document.querySelector('.overlay-gallery');
   var galleryClose = document.querySelector('.overlay-gallery-close');
 
-
-  function keyHandler(evt) {
-    switch (evt.keyCode) {
-      case key.LEFT :
-        console.log('left');
-        break;
-      case key.RIGHT :
-        console.log('right');
-        break;
-      case key.ESC :
-        hideGallery();
-        break;
-      default:
-        break;
-    }
-  }
+      function hideGallery() {
+            galleryElement.classList.add('invisible');
+            galleryClose.removeEventListener('click', closeButton);
+            document.body.removeEventListener('keydown', keyHandler);
+          }
 
   function closeButton(evt) {
-    evt.preventDefault();
-    hideGallery();
-  }
-
-  function hideGallery() {
-    galleryElement.classList.add('invisible');
-    galleryClose.removeEventListener('click', closeButton);
-    document.body.removeEventListener('keydown', keyHandler);
-  }
+        evt.preventDefault();
+        hideGallery();
+      }
 
   function showGallery() {
-    galleryElement.classList.remove('invisible');
-    galleryClose.addEventListener('click', closeButton);
-    document.body.addEventListener('keydown', keyHandler);
-  }
+        galleryElement.classList.remove('invisible');
+        galleryClose.addEventListener('click', closeButton);
+        document.body.addEventListener('keydown', keyHandler);
+      }
 
+  function keyHandler(evt) {
+        switch (evt.keyCode) {
+              case key.LEFT :
+                    console.log('left');
+                    break;
+                  case key.RIGHT :
+                    console.log('right');
+                    break;
+                  case key.ESC :
+                    hideGallery();
+                    break;
+                  default: break;
+                }
+      }
 
   galleryContainer.addEventListener('click', function(evt) {
-    evt.preventDefault();
-
-    if (evt.target.parentNode.classList.contains('photogallery-image')) {
-      showGallery();
-    }
-  });
+        evt.preventDefault();
+        console.log('try to open gallery');
+        if (evt.target.parentNode.classList.contains('photogallery-image')) {
+              showGallery();
+            }
+      })
 
 })();
