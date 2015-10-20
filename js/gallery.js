972309d97cc11c194aee5b1f75ac8c2f4a7a3f18
zweizwei <elena.ganenkova@gmail.com>
2015-10-20 14:22:44 +0300

    Task 5/1 - add gallery js - with debugging

---
+++ js/gallery.js
@@ -0,0 +1,53 @@
+(function() {
+
+          var key = {
+            'LEFT' : 37,
+            'RIGHT' : 39,
+            'ESC' : 27
+      }
+
+  var galleryContainer = document.querySelector('.photogallery');
+  var galleryElement = document.querySelector('.overlay-gallery');
+  var galleryClose = document.querySelector('.overlay-gallery-close');
+
+      function hideGallery() {
+            galleryElement.classList.add('invisible');
+            galleryClose.removeEventListener('click', closeButton);
+            document.body.removeEventListener('keydown', keyHandler);
+          }
+
+  function closeButton(evt) {
+        evt.preventDefault();
+        hideGallery();
+      }
+
+  function showGallery() {
+        galleryElement.classList.remove('invisible');
+        galleryClose.addEventListener('click', closeButton);
+        document.body.addEventListener('keydown', keyHandler);
+      }
+
+  function keyHandler(evt) {
+        switch (evt.keyCode) {
+              case key.LEFT :
+                    console.log('left');
+                    break;
+                  case key.RIGHT :
+                    console.log('right');
+                    break;
+                  case key.ESC :
+                    hideGallery();
+                    break;
+                  default: break;
+                }
+      }
+
+  galleryContainer.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        console.log('try to open gallery');
+        if (evt.target.parentNode.classList.contains('photogallery-image')) {
+              showGallery();
+            }
+      })
+
+})();
