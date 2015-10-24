df1ff74efd2ba763aaf76c199cc647b53e53d263
zweizwei <elena.ganenkova@gmail.com>
2015-10-24 20:00:22 +0300

    task 6.1 - first class review.js

---
+++ js/review.js
@@ -0,0 +1,57 @@
+
+'use strict';
+
+(function() {
+
+  var ratingClassName = {
+    '4': 'review-rating-one',
+    '5': 'review-rating-two',
+    '6': 'review-rating-three',
+    '7': 'review-rating-four',
+    '8': 'review-rating-five'
+  };
+
+
+  var Review = function(data) {
+    this.data = data;
+    this.element = null;
+  };
+
+  Review.prototype.render = function(container) {
+    var reviewTemplate = document.getElementById('review-template');
+    var newReviewElement = reviewTemplate.content.children[0].cloneNode(true);
+
+    newReviewElement.querySelector('.review-rating').classList.add(ratingClassName[this.data.rating]);
+    newReviewElement.querySelector('.review-text').textContent = this.data.description;
+
+    if (this.data.author.picture) {
+      var authorPicture = new Image();
+      authorPicture.src = this.data.author.picture;
+
+      authorPicture.addEventListener('load', function() {
+        newReviewElement.replaceChild(authorPicture, newReviewElement.childNodes[1]);
+        authorPicture.classList.add('review-author');
+        authorPicture.width = 124;
+        authorPicture.height = 124;
+      });
+
+
+      authorPicture.addEventListener('error', function() {
+        newReviewElement.classList.add('review-load-failure');
+      });
+
+      container.appendChild(newReviewElement);
+
+      this.element = newReviewElement;
+    }
+  };
+
+  Review.prototype.unrender = function() {
+    this.element.parentNode.removeChild(this.element);
+    this.element = null;
+  };
+
+  window.Review = Review;
+
+})();
+
